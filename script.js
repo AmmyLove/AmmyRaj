429f907192b7aaca619f7d4d251ece4722ff4840
AmmyLove <128863988+AmmyLove@users.noreply.github.com>
2023-08-12 14:05:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const readMoreLinks = document.querySelectorAll(".read-more");
+
+    readMoreLinks.forEach((readMoreLink) => {
+        readMoreLink.addEventListener("click", (e) => {
+            e.preventDefault();
+            const paragraph = readMoreLink.parentElement;
+            const moreText = paragraph.querySelector(".moreText");
+            paragraph.classList.toggle("show-more");
+            if (paragraph.classList.contains("show-more")) {
+                readMoreLink.innerText = "Read Less";
+            } else {
+                readMoreLink.innerText = "Read More";
+            }
+        });
+    });
+});
+
+
+
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        img.src = image.src;
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active');
+})
+
+
